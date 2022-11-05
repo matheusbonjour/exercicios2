30661d0335f8e9135b792e8268d800615f0b2af9
matheusbonjour <matheusbonjour@users.noreply.github.com>
2022-11-05 13:08:49 -0300

    aulas_5/11_parte1

---
+++ pages/basicos/jsx2.js
@@ -0,0 +1,20 @@
+export default function jsx2() {
+    const array = [
+        <li>Matheus</li>,
+        <li>Gabigol</li>,
+        <li>Arrascaeta</li>,
+    ]
+    
+    const conteudo = (
+        <div>
+            <h1>JSX #02</h1>
+        </div>
+    )
+    return array
+    
+}
+
+// HTML, CSS e JS - Tecnologias
+
+// Componentes (HTML, CSS e JS)
+// PENSAMOS DE FORMA INTEGRADA, DENTRO DOS COMPONENTES TEMOS AS 3 TECNOLOGIAS TRABALHABDO JUNTAS
